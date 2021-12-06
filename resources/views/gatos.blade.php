bde2627c4c87a2109dac9a2ccea1c6414403defb
gastonnoda1 <95653279+gastonnoda1@users.noreply.github.com>
2021-12-06 17:27:44 -0300

    Delete resources directory

--- resources/views/gatos.blade.php
+++
@@ -1,7 +0,0 @@
-
-@extends('welcome')
-<h1 class="saludos">datos de prueba</h1> 
-<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-<input type="text" name="nombre" id="nombre" class="nombre" >
-
-
